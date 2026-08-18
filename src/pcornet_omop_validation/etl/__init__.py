4317b74446900d946e75c7c8ea67ab94e4b5239b
Alireza Vafaei Sadr <vafaei.sadr@gmail.com>
2026-08-17 20:47:52 -0400

    Expose ETL database and schema helpers

--- src/pcornet_omop_validation/etl/__init__.py
+++ src/pcornet_omop_validation/etl/__init__.py
@@ -1,6 +1,19 @@
 """Audited PCORnet-to-OMOP ETL orchestration."""
 
 from .config import EtlConfig, load_etl_config
+from .database import DatabaseStatus, check_connection
+from .manifest import write_run_manifest
 from .preflight import PreflightResult, run_preflight
+from .schema import SchemaResult, apply_omop_schema
 
-__all__ = ["EtlConfig", "PreflightResult", "load_etl_config", "run_preflight"]
+__all__ = [
+    "DatabaseStatus",
+    "EtlConfig",
+    "PreflightResult",
+    "SchemaResult",
+    "apply_omop_schema",
+    "check_connection",
+    "load_etl_config",
+    "run_preflight",
+    "write_run_manifest",
+]
